92358cf18d2c0162d79dd0d9475f8ce5f3be4e78
Andrea Santos <deasantos.dev@gmail.com>
2025-11-18 22:08:10 -0300

    Create account

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+const myModal = new bootstrap.Modal('#register-modal');
+
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if(email.length < 5) {
+       alert(`Preencha o campoo com um e-mail válido.`);
+    }
+
+    if(password.length < 4) {
+        alert(`A senha deve ter no mínimo 4 dígitos.`);
+        return;
+    }
+
+    saveAccount({ 
+        login: email, 
+        password: password,
+        transactions: [],
+    });
+
+    myModal.hide();
+
+    alert("Conta criada com sucesso!");
+    
+});
+
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+
+}
